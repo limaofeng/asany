e350e9af1ab4dc67401ce14a29954b2bdba8f8f0
limaofeng <limaofeng@msn.com>
2023-08-03 18:09:11 +0800

    feat: asany 部署文件

---
+++ clear.sh
@@ -0,0 +1,5 @@
+rm -rf components/elasticsearch/certs
+rm -rf components/minio
+rm -rf components/mysql
+rm -rf components/mongodb
+rm -rf components/redis
---
+++ install.sh
@@ -0,0 +1,3 @@
+
+
+dotenv envsubst < components/openim/config/config.template.yaml > components/openim/config/config.yaml
